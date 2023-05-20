a95fbb05cc5ebb5b83c259b1f7331746528f285e
Saveliy <lekhtins@mail.ru>
2023-05-20 23:25:22 +0300

    Настройка шаблонов. Соблюдение принципа DRY.

--- products/views.py
+++ products/views.py
@@ -4,8 +4,15 @@ from django.shortcuts import render
 
 
 def index(request):
-    return render(request, 'products/index.html')
+    context = {
+        'title': "Test Title",
+        'is_promotion': False,
+    }
+    return render(request, 'products/index.html', context)
 
 
 def products(request):
+    context = {
+        'title': 'Store - Каталог',
+    }
     return render(request, 'products/products.html')
--- store/settings.py
+++ store/settings.py
@@ -120,6 +120,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
+"""Для работы со статикой необходимо добавить STATICFILES_DIRS"""
+
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     BASE_DIR / 'static'
