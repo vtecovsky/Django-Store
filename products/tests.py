fcde3f4098ca9a679b904dac0c1e2cfa05e0323f
Saveliy <s.lekhtin@innopolis.university>
2023-07-21 16:43:46 +0300

    add first test

--- products/tests.py
+++ products/tests.py
@@ -1,3 +1,12 @@
 from django.test import TestCase
+from django.urls import reverse
 
-# Create your tests here.
+
+class IndexViewTestCase(TestCase):
+    def test_view(self):
+        path = reverse('index')
+        response = self.client.get(path)
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.context_data['title'], "Store")
+        self.assertTemplateUsed(response, "products/index.html")
